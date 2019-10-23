8fb907db16a2ba48ccb78f6b3b46e4218aa4ffd4
root <root@localhost.localdomain>
2019-10-23 01:39:02 -0700

    week7

--- week7/code/file/demo2.c
+++ week7/code/file/demo2.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 #include <stdlib.h>
-
 #include <sys/types.h>
 
- 
-
-char buf[] = {"write to stdout\n"};
+char buf[] = {"1234 stdout\n"};
 
 int main()
-
 {
-
 pid_t id;
-
  
-
 if(write(STDOUT_FILENO, buf, sizeof(buf) - 1) != sizeof(buf) - 1)
-
-perror("can't write");
-
-printf("printf ");
-
+	perror("can't write");	
+printf("ABCDEF ");
 id = fork();
-
 if(id < 0)
-
-perror("fork error");
-
- 
-
+	perror("fork error");
 exit(0);
-
 }
